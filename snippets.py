41124bd6982ee4e908fd6cb967a16245bbf6a1a3
McKoy <ec2-user@ip-172-31-15-5.us-east-2.compute.internal>
2018-10-18 14:12:16 +0000

    Added parser structure

--- snippets.py
+++ snippets.py
@@ -33,7 +33,23 @@ def main():
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet")
     
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Get a snippet")
+    get_parser.add_argument("name", help="Name of snippet")
+    
     arguments = parser.parse_args()
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+    
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retreived snippet: {!r}".format(snippet))
+        
+    
 
 if __name__ == "__main__":
     main()
