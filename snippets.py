b56de709f2d80c2f86f5341248498b49ed1d2fac
mckoyd <dominick.mckoy@gmail.com>
2018-10-18 20:39:14 -0400

    implemented search method

--- snippets.py
+++ snippets.py
@@ -39,6 +39,28 @@ def get(name):
     logging.debug("Retrieved snippet successfully")
     return snippet[0]
 
+def catalog():
+    """
+    Retrieves all keywords.
+    """
+    logging.debug("Retrieving all keywords")
+    with connection, connection.cursor() as cursor:
+        cursor.execute("SELECT keyword FROM snippets ORDER BY keyword")
+        keywords = cursor.fetchall()
+    logging.debug("Retrieved all keywords successfully")
+    return keywords
+
+def search(query):
+    """
+    Retrieves names and snippets based on search query.
+    """
+    logging.debug("Retrieving all keywords and snippets matching the following search query: {!r}".format(query))
+    with connection, connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM snippets WHERE message LIKE '%{}%'".format(query))
+        entries = cursor.fetchall()
+    logging.debug("Retrieved all entries matching query.")
+    return entries
+
 def main():
     """Main function"""
     logging.info("Constructing parser")
@@ -53,21 +75,37 @@ def main():
     
     # Subparser for the get command
     logging.debug("Constructing get subparser")
-    get_parser = subparsers.add_parser("get", help="Get a snippet")
+    get_parser = subparsers.add_parser("get", help="Get a snippet by name")
     get_parser.add_argument("name", help="Name of snippet")
-    
+
+    # Subparser for the catalog command
+    logging.debug("Constructing catalog subparser")
+    get_parser = subparsers.add_parser("catalog", help="Get all names and snippets")
+
+    # Subparser for the search command
+    logging.debug("Constructing search subparser")
+    get_parser = subparsers.add_parser("search", help="Search for snippets based on query")
+    get_parser.add_argument("query", help="Searches for snippets that match the given query")
+
     arguments = parser.parse_args()
     arguments = vars(arguments)
     command = arguments.pop("command")
-    
+
     if command == "put":
         name, snippet = put(**arguments)
         print("Stored {!r} as {!r}".format(snippet, name))
     elif command == "get":
         snippet = get(**arguments)
         print("Retreived snippet: {!r}".format(snippet))
-        
-    
+    elif command == "catalog":
+        keywords = catalog()
+        results = []
+        for i in range(len(keywords)):
+            results.append(keywords[i][0])
+        print("Retrieved all keywords: {!r}".format(results))
+    elif command == "search":
+        entries = search(**arguments)
+        print("Retrieved all entries matching given query: {!r}".format(entries))
 
 if __name__ == "__main__":
     main()
